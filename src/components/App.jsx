23934b15499db1f804a9435948c143a24f1402c6
ivanka08 <ivankahudic@gmail.com>
2023-11-14 22:43:47 +0200

    End

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Section from "./Sections/Section";
+import Statistics from "./Statistics/Statistics";
+import Notification from "./Notification/Notification";
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = (evt) => {
+    // console.log(evt.target);
+    const { name } = evt.target;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+    const positivePercentage = Math.round((this.state.good / total) * 100);
+    return positivePercentage;
+
+  };
+
+render() {
+
+  const { good, neutral, bad } = this.state;
+  const total = this.countTotalFeedback();
+  const positivePercentage = this.countPositiveFeedbackPercentage();
+  
+  return <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions 
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+            <Notification message="There is no feedback"/>
+        )}
+      </Section>
+  </div>
+  }
+}
+export default App;
---
+++ src/components/FeedbackButtons/FeedbackButtons.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import classNames from 'classnames/bind';
+
+
+const Button = ({ children, onLeaveFeedback, type, extraClass, name }) => {
+    return <button
+      onClick={onLeaveFeedback}
+      type={type}
+      name={name}
+    >{children}
+    </button>
+  }
+  
+  export default Button;
+
+  Button.propTypes = {
+    children: PropTypes.string.isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+    type: PropTypes.string,
+    extraClass: PropTypes.string,
+    name: PropTypes.string.isRequired,
+  }
+  
